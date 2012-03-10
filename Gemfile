5f774fa6e46d25ea1b3640388034f849666cf439
Daniel Romero <infoslack@gmail.com>
2012-03-10 20:58:29 -0300

    configurado mongodb em config/boot.rb

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source :rubygems
 
 gem "sinatra"
+gem "mongo_mapper"
+gem "bson_ext"
 
 group :development do
   gem "shotgun"
--- config/boot.rb
+++ config/boot.rb
@@ -7,3 +7,14 @@ Bundler.require
 Dir[File.expand_path("../../app/routes/**/*.rb", __FILE__)].each do |file|
   require file
 end
+
+set :views, File.expand_path("../../app/views", __FILE__)
+set :public_folder, File.expand_path("../../public", __FILE__)
+
+MongoMapper.config = {
+  :development  => {"uri" => "mongodb://localhost/todo_development"},
+  :test         => {"uri" => "mongodb://localhost/todo_test"},
+  :production   => {"uri" => ENV["MONGOLAB_URI"]}
+}
+
+MongoMapper.connect(Sinatra::Base.environment)
